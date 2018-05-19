917756c0990310fe8a9960deb440f8a3b7362064
Michał Budzyński <michal@virtualdesign.pl>
2018-05-19 21:36:00 +0100

    :dizzy: fuck babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
@@ -17,9 +16,6 @@ export default {
       customResolveOptions: {
         moduleDirectory: 'node_modules'
       }
-    }),
-    babel({
-      exclude: 'node_modules/**'
     }),
     serve({
       contentBase: ['dist']
