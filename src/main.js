4db9683ecf84eaf1f124fbbd10b8472ef5297a32
Michał Budzyński <michal@virtualdesign.pl>
2018-05-19 22:50:00 +0100

    :bouquet: test game behaviour

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { Rom } from './rom';
+import { Chip8 } from './chip8';
 
 const rom = new Rom({
     path: 'roms/invaders.rom'
 });
 
-rom.load().then((content) => {
-    console.log(content);
+const chip8 = new Chip8();
+rom.load().then((program) => {
+    console.log(program);
+    chip8.load_program(program);
+    chip8.start();
 });
