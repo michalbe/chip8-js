02de49eb787a26fe692e1ccb96acd4ca76302efd
Michał Budzyński <michal@virtualdesign.pl>
2018-07-20 23:25:00 +0100

    :tiger: webgl renderer as default

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 import { Rom } from './rom';
 import { Chip8 } from './chip8';
 // import { Renderer } from './renderer-ascii';
-// import { Renderer } from './renderer-webgl';
-import { Renderer } from './renderer';
+import { Renderer } from './renderer-webgl';
+// import { Renderer } from './renderer';
 
 const renderer = new Renderer();
 
 const rom = new Rom({
-    path: 'roms/invaders.rom'
+    // path: 'roms/invaders.rom'
+    path: 'roms/tetris.rom'
 });
 
 const chip8 = new Chip8({ renderer });
